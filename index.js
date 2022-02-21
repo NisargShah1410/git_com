83c590d89549e17cb26f5d18ad2fb7974fb772e2
NisargShah1410 <nisargshah01410@gmail.com>
2022-02-21 12:38:33 +0000

    second

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var child_process = require('child_process');
 
 try {
 
-  exec(`git config user.name "NisargShah1410"`,
+  exec(`git config user.name "NisargShah1410" &&  git config user.email "nisargshah01410@gmail.com" && git clone https://github.com/NisargShah1410/git_com.git && cd git_com && git log`,
   (error, stdout, stderr) => {
       console.log(stdout);
       console.log(stderr);
