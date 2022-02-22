d0c83f018c2c340fd5e422dff01bcc2df06108ef
NisargShah1410 <nisargshah01410@gmail.com>
2022-02-22 10:25:32 +0000

    eighth commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ try {
   const repository = core.getInput('repository');
   let repo= repository;
 
-  exec(`git clone https://github.com/${handle}/${repo}.git && cd ${repo} && git log --pretty=format:"%cn %s %b %N"`,
+  exec(`git clone https://github.com/${handle}/${repo}.git && cd ${repo} && git log --pretty=format:"%n %nCommited by:%an %nCommit comment:%s %nFiles changed:" --name-only`,
   (error, stdout, stderr) => {
       console.log(stdout);
       console.log(stderr);
